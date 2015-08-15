b40fc733d5caa9a1bb5fdc5eb4dcd70ead116378
Alex Laughnan <alex.laughnan@metaltoad.com>
2015-08-15 11:13:56 -0700

    Merged the two template files together.

--- block-juicer.tpl.php
+++
@@ -1,1 +0,0 @@
-<ul class="juicer-feed" data-feed-id="<?php print $feed_id; ?>"></h1></ul>
--- templates/block-juicer.tpl.php
+++ templates/block-juicer.tpl.php
@@ -1,3 +1,1 @@
-<script src="//assets.juicer.io/embed.js" type="text/javascript"></script>
-<link href="//assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
-<ul class="juicer-feed" data-feed-id="alexlaughnan"><h1 class="referral"><a href="http://www.juicer.io">Powered by Juicer</a></h1></ul>
+<ul class="juicer-feed" data-feed-id="<?php print $feed_id; ?>"></h1></ul>
